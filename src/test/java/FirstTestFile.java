f275ba0e2066bd86ac2d904936bf88c70e65cb88
steph <stephane.mg@gmail.com>
2022-03-11 07:26:16 +0100

    init

--- src/test/java/FirstTestFile.java
+++ src/test/java/FirstTestFile.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -5,17 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class FirstTestFile {
-    public String baseUrl = "http://demo.guru99.com/test/newtours/";
+
 
 
     public WebDriver driver ;
 
-    @Test
-    public void verifyHomepageTitle() {
+    @BeforeAll
+    public static void set() {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\steph\\IdeaProjects\\demo\\Selenium_Tutorials\\chromedriver.exe");
+    }
 
+    @BeforeEach
+    public void launchBrowser() {
         System.out.println("launching chrome browser");
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verifyHomepageTitle() {
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
         driver.get(baseUrl);
         String expectedTitle = "Welcome: Mercury Tours";
         String actualTitle = driver.getTitle();
