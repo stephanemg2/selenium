94192b59730babc89da2ef8f08d7488b397e0bb5
steph <stephane.mg@gmail.com>
2022-03-12 06:40:40 +0100

    init

--- src/main/java/ActionMouseOver.java
+++ src/main/java/ActionMouseOver.java
@@ -3,15 +3,24 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActionMouseOver {
 
     public static void main(String[] args) {
         String baseUrl = "http://demo.guru99.com/test/newtours/";
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\steph\\IdeaProjects\\demo\\Selenium_Tutorials\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\steph\\IdeaProjects\\demo\\Selenium_Tutorials\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get(baseUrl);
+        //click consent
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//*[@id=\"gdpr-consent-notice\"]")));
+        WebElement until = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[id='save']")));
+        until.click();
+        //sort de l'iframe
+        driver.switchTo().defaultContent();
         WebElement link_Home = driver.findElement(By.linkText("Home"));
         WebElement td_Home = driver
                 .findElement(By
